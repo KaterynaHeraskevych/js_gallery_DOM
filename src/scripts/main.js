653aa7feec1f3162bc432255dfc1c4c353d23eeb
Kateryna <katiaherascevich@gmail.com>
2025-11-26 13:37:58 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const listItems = document.querySelectorAll('.list-item');
+const bigImg = document.querySelector('#largeImg');
+
+listItems.forEach((el) => {
+  el.addEventListener('click', (e) => e.preventDefault(), { capture: true });
+
+  el.addEventListener('click', (e) => {
+    if (e.currentTarget.tagName === 'LI') {
+      bigImg.src = e.currentTarget.children[0].href;
+    }
+  });
+});
